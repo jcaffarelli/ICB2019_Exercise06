f3c041aa2dd0a8cdf508dbeb4d3abc3e9ea1593e
John Caffarelli <jcaffare@nd.edu>
2019-11-01 11:02:14 -0400

    setting up document in R

---
+++ Ex_6.R
@@ -0,0 +1,10 @@
+#Exercise 06 Tutorial 
+#setting John's working directory
+setwd("C:/Users/jcaff/Documents/Notre Dame/senior/fall_19/Biocomputing/R/Tutorial/Ex_6")
+
+#replicating head function (Unix) in R
+
+
+##IRIS##
+#Loading iris.csv into R
+
