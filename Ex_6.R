c90cf8c7c8bf848366e5efdd2f77ad2f05a82aba
John Caffarelli <jcaffare@nd.edu>
2019-11-01 11:12:48 -0400

    updating working directory and loading iris

--- Ex_6.R
+++ Ex_6.R
@@ -1,10 +1,11 @@
 #Exercise 06 Tutorial 
 #setting John's working directory
-setwd("C:/Users/jcaff/Documents/Notre Dame/senior/fall_19/Biocomputing/R/Tutorial/Ex_6")
+setwd("C:/Users/jcaff/Documents/Notre Dame/senior/fall_19/Biocomputing/R/Tutorial/Ex_6/ICB2019_Excercise06")
 
 #replicating head function (Unix) in R
 
 
 ##IRIS##
 #Loading iris.csv into R
+iris<-read.csv("iris.csv",header = TRUE)
 
