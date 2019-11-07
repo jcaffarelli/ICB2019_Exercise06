6f8e6c18f4c728f134c3f23119920ce155b9d175
John Caffarelli <jcaffare@nd.edu>
2019-11-07 10:10:52 -0500

    adding changes to document

--- Ex_6.R
+++ Ex_6.R
@@ -10,11 +10,26 @@ setwd("C:/Users/jcaff/Documents/Notre Dame/senior/fall_19/Biocomputing/R/Tutoria
 iris<-read.csv("iris.csv",header = TRUE)
 
 #print last 2 rows in the last 2 columns 
+dim(iris)
 iris[149:150,4:5]
 
-#the number of observations for each species 
-
+##the number of observations for each species 
+head(iris)
+#find unique list of species names
+species<-iris[,5]
+speciesUniq<-unique(species)
+head(speciesUniq)
+#find number of oberservations for setosa
+setosaObs<-iris[iris$Species=="setosa",]
+dim(setosaObs)
+#find number of observations for versicilor
+versicolorObs<-iris[iris$Species=="versicolor",]
+dim(versicolorObs)
+#find number of observations for virginica
+virginicaObs<-iris[iris$Species=="virginica",]
+dim(virginicaObs)
 #rows with Sepal.Width > 3.5
-
+Sepal3.5<-iris[iris$Sepal.Width>3.5,]
+head(Sepal3.5)
 #write the data for setosa into "setosa.csv"
 
